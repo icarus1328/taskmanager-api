b8b2538f721a761f4674e3fbbfe4d209f499a6a4
light <revibe001@gmail.com>
2026-03-20 19:11:40 +0530

    chore: Initial project setup with libraries

---
+++ app/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args = {"check_same_thread": False}
+)
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False,bind=engine)
---
+++ app/models.py
@@ -0,0 +1,35 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    id       = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), nullable=False, unique=True)
+    email    = Column(String(100), nullable=False, unique=True)
+    
+    tasks = relationship("Tasks", back_populates="author")
+    
+    def __repr__(self):
+        return f"<User ID: {self.user_id} Username: {self.username}"
+    
+class Tasks(Base):
+    __tablename__ = 'tasks'
+    
+    id          = Column(Integer, primary_key=True, nullable=False,index=True)
+    title       = Column(String(150), nullable=False)
+    description = Column(String(200), nullable=True)
+    completed   = Column(Boolean, default=False)
+    due_date    = Column(DateTime(timezone=True), nullable=False)
+    created_at  = Column(DateTime(timezone=False), server_default=func.now())
+    updated_at  = Column(DateTime(timezone=False), server_default=func.now())
+    user_id  = Column(Integer, ForeignKey("users.id"), nullable=False)
+    
+    author = relationship("User", back_populates="tasks")
+    
+    def __repr__(self):
+        return f"<Task: {self.title} Completed:{self.completed}>"
+    
